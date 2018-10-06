4b098d7d02763022229af54de8cb0e63fb098070
Bryce Eadie <bryce.eadie@gmail.com>
2018-10-06 07:33:27 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,14 +1,13 @@
 class Dog
   def initialize(name, breed)
     @name = name
+    @breed = breed
   end
   def name=(name)
     @name = name
   end
   def name
     @name
-  end
-    @breed = breed
   end
   def breed=(breed="Mutt")
     @breed = breed
